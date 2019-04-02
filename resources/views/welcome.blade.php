4239dd015fc406a552afdc347cc405af62bf7a7d
Ana Urquiola <anitaurquiola28@gmail.com>
2019-04-02 20:45:42 -0300

    imagen home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <div class="container">
 
       <div class="intro-img">
-        <img src="img/images/home-img.png" alt="" class="mg-fluid">
+        <img src="{{ asset('img/images/home-img.png') }}" alt="" class="mg-fluid">
       </div>
 
       <div class="intro-info">
