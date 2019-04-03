0ccd14c6dba391047294cd27cc9d38cd969a3dc2
Ana Urquiola <ana@presisconsultores.com name = Ana Urquiola>
2019-04-03 14:12:05 -0300

    favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,7 +8,7 @@
   <meta content="" name="description">
 
   <!-- Favicons -->
-  <link href="{{ asset('img/logos/favicon.ico') }}" rel="icon">
+  <link href="{{ asset('img/logos/favicon.png') }}" rel="icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
